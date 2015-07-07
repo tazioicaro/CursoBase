86dd41b99160b189ee4f4fb765eea69f8f3857fb
tazio.fernandes <tazio.fernandes@gmail.com>
2015-07-07 14:36:52 -0300

    Criação da parte inferior da tabela

--- PedidoVenda/src/main/java/com/algaworks/cursojavaee/controller/CadastroPedidoBean.java
+++ PedidoVenda/src/main/java/com/algaworks/cursojavaee/controller/CadastroPedidoBean.java
@@ -1,8 +1,14 @@
 package com.algaworks.cursojavaee.controller;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+
+@ManagedBean
+@ViewScoped
 public class CadastroPedidoBean {
 	
 	private List<Integer> itens;
