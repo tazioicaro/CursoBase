d56fa87b998214f797194bd808834804dcb51046
tazio.fernandes <tazio.fernandes@gmail.com>
2015-07-10 16:50:42 -0300

    Falta ajustar o Weld com o tomcat

---
+++ PedidoVenda/src/main/java/com/algaworks/cursojavaee/controller/PrecoProdutoBean.java
@@ -0,0 +1,12 @@
+package com.algaworks.cursojavaee.controller;
+
+import javax.inject.Named;
+
+@Named("meuBean")
+public class PrecoProdutoBean {
+
+	
+	public double getPreco(){
+		return 10.45;
+	}
+}
