13167265336a5f478a84556555e5e25a20d24a79
Tazio <tazio.fernandes@gmail.com>
2015-07-29 21:23:05 -0300

    Ajuste JPA e Cadastro de Clientes

---
+++ PedidoVenda/src/main/java/com/algaworks/cursojavaee/model/Cliente.java
@@ -0,0 +1,83 @@
+package com.algaworks.cursojavaee.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente  implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	private String email;
+	private String documntoReceitaFederal;
+	private TipoPessoa tipoPessoa;
+	private List<Endereco> enderecos = new ArrayList<>();
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDocumntoReceitaFederal() {
+		return documntoReceitaFederal;
+	}
+	public void setDocumntoReceitaFederal(String documntoReceitaFederal) {
+		this.documntoReceitaFederal = documntoReceitaFederal;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+	public TipoPessoa getTipoPessoa() {
+		return tipoPessoa;
+	}
+	public void setTipoPessoa(TipoPessoa tipoPessoa) {
+		this.tipoPessoa = tipoPessoa;
+	}
+	
+	
+	
+
+}
---
+++ PedidoVenda/src/main/java/com/algaworks/cursojavaee/model/TipoPessoa.java
@@ -0,0 +1,7 @@
+package com.algaworks.cursojavaee.model;
+
+public enum TipoPessoa {
+	
+	FISICA, JURIDICA;
+
+}
