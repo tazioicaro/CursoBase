2deda1824e47636045b72313cbe8a1e01eee2812
Tazio <tazio.fernandes@gmail.com>
2015-07-30 12:09:56 -0300

    Completando registro classes

--- PedidoVenda/src/main/java/com/algaworks/cursojavaee/model/Categoria.java
+++ PedidoVenda/src/main/java/com/algaworks/cursojavaee/model/Categoria.java
@@ -4,51 +4,47 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Usuario implements Serializable {
-
+public class Categoria implements Serializable{
+	
 	private static final long serialVersionUID = 1L;
-	
+
 	private Long id;
-	private String nome;
-	private String email;
-	private String senha;
-	private List<Grupo> grupos = new ArrayList<>();
-	
+	private String descricao;
+	private Categoria categoriaPai;
+	private List<Categoria> subcategorias = new ArrayList<>();
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
-	public String getNome() {
-		return nome;
+
+	public String getDescricao() {
+		return descricao;
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
-	
-	public String getEmail() {
-		return email;
+
+	public Categoria getCategoriaPai() {
+		return categoriaPai;
 	}
-	public void setEmail(String email) {
-		this.email = email;
+
+	public void setCategoriaPai(Categoria categoriaPai) {
+		this.categoriaPai = categoriaPai;
 	}
-	
-	public String getSenha() {
-		return senha;
+
+	public List<Categoria> getSubcategorias() {
+		return subcategorias;
 	}
-	public void setSenha(String senha) {
-		this.senha = senha;
+
+	public void setSubcategorias(List<Categoria> subcategorias) {
+		this.subcategorias = subcategorias;
 	}
-	
-	public List<Grupo> getGrupos() {
-		return grupos;
-	}
-	public void setGrupos(List<Grupo> grupos) {
-		this.grupos = grupos;
-	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -56,6 +52,7 @@ public class Usuario implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -64,7 +61,7 @@ public class Usuario implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Usuario other = (Usuario) obj;
+		Categoria other = (Categoria) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -72,5 +69,6 @@ public class Usuario implements Serializable {
 			return false;
 		return true;
 	}
+
 
 }
--- PedidoVenda/src/main/java/com/algaworks/cursojavaee/model/Grupo.java
+++ PedidoVenda/src/main/java/com/algaworks/cursojavaee/model/Grupo.java
@@ -1,54 +1,38 @@
 package com.algaworks.cursojavaee.model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
-public class Usuario implements Serializable {
-
+public class Grupo implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Long id;
 	private String nome;
-	private String email;
-	private String senha;
-	private List<Grupo> grupos = new ArrayList<>();
-	
+	private String descricao;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	public String getEmail() {
-		return email;
+
+	public String getDescricao() {
+		return descricao;
 	}
-	public void setEmail(String email) {
-		this.email = email;
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
-	
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-	
-	public List<Grupo> getGrupos() {
-		return grupos;
-	}
-	public void setGrupos(List<Grupo> grupos) {
-		this.grupos = grupos;
-	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -56,6 +40,7 @@ public class Usuario implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -64,7 +49,7 @@ public class Usuario implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Usuario other = (Usuario) obj;
+		Grupo other = (Grupo) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
