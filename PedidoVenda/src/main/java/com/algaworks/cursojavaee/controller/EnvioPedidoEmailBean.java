f6eab4f49e141327a3546372098536a07c28b9e0
tazio.fernandes <tazio.fernandes@gmail.com>
2016-05-13 16:35:59 -0300

    Configurando o Velocity tool na classe envioPedidoEmailBean

--- PedidoVenda/src/main/java/com/algaworks/cursojavaee/controller/EnvioPedidoEmailBean.java
+++ PedidoVenda/src/main/java/com/algaworks/cursojavaee/controller/EnvioPedidoEmailBean.java
@@ -1,15 +1,19 @@
 package com.algaworks.cursojavaee.controller;
 
 import java.io.Serializable;
+import java.util.Locale;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.apache.velocity.tools.generic.NumberTool;
+
 import com.algaworks.cursojavaee.model.Pedido;
 import com.algaworks.cursojavaee.util.jsf.FacesUtil;
 import com.algaworks.cursojavaee.util.mail.Mailer;
 import com.outjected.email.api.MailMessage;
+import com.outjected.email.impl.templating.velocity.VelocityTemplate;
 
 @Named
 @RequestScoped
@@ -24,7 +28,11 @@ public class EnvioPedidoEmailBean implements Serializable {
 	@PedidoEdicao
 	private Pedido pedido;
 	
-	
+	/**
+	 * Usando o template do Apache velocity
+	 * para isso é necessário passar o objeto pedido, para que as chamadas EL
+	 * possam funcionar da classe para o template
+	 */
 	
 	
 	public void enviarPedido(){
@@ -32,7 +40,10 @@ public class EnvioPedidoEmailBean implements Serializable {
 		MailMessage message = mailer.novaMensagem();
 		
 		message.to(this.pedido.getCliente().getEmail()).subject("Pedido" + this.pedido.getId())
-		.bodyHtml("<strong> Valor total:</strong>" + this.pedido.getValorTotal())
+		.bodyHtml(new VelocityTemplate(getClass().getResourceAsStream("/emails/pedido.template")))
+		.put("pedido", this.pedido)
+		.put("numberTool", new NumberTool())
+		.put("locale", new Locale("pt", "BR"))
 		.send();
 		FacesUtil.addInforMessage("Pedido enviado por email com sucesso!");
 	}
