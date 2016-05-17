d4a96b112588006fd916ff97c4f7cc9474dc20bb
tazio.fernandes <tazio.fernandes@gmail.com>
2016-05-17 13:37:43 -0300

    Protejendo componentes em relação as permissões de usuários por grupos

--- PedidoVenda/src/main/java/com/algaworks/cursojavaee/security/Seguranca.java
+++ PedidoVenda/src/main/java/com/algaworks/cursojavaee/security/Seguranca.java
@@ -1,7 +1,9 @@
 package com.algaworks.cursojavaee.security;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,39 +11,60 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 @Named
 @RequestScoped
 public class Seguranca {
-	
+
 	/**
-	 *  @return
-	 *  Classe que vai exibir no nome do usuário nas páginas
+	 * @return Classe que vai exibir no nome do usuário nas páginas
+	 * 
 	 */
-	
-	public String getNomeUsuario(){
-		
+
+	@Inject
+	private ExternalContext externalContext;
+
+	public String getNomeUsuario() {
+
 		String nome = null;
-		
+
 		UsuarioSistema usuarioLogado = getUsuarioLogado();
-		
-		if( usuarioLogado != null){
-		nome = usuarioLogado.getUsuario().getNome();
-		
+
+		if (usuarioLogado != null) {
+			nome = usuarioLogado.getUsuario().getNome();
+
 		}
 		return nome;
-		
+
 	}
 
 	private UsuarioSistema getUsuarioLogado() {
 		UsuarioSistema usuario = null;
-		
-		//Convertendo o usuário logado JSF para Spring
-		
-		UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken)
-		FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
-		
-		if (auth != null && auth.getPrincipal() != null){
-			
+
+		// Convertendo o usuário logado JSF para Spring
+
+		UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken) FacesContext
+				.getCurrentInstance().getExternalContext().getUserPrincipal();
+
+		if (auth != null && auth.getPrincipal() != null) {
+
 			usuario = (UsuarioSistema) auth.getPrincipal();
 		}
 		return usuario;
+	}
+
+	/**
+	 * Garanti para que apenas os Funcionáro com permissão de Vendedores ou
+	 * Administradores possam emitir ou cancelar um pedido o perfil de auxiliar
+	 * não poderá fazer isso.
+	 */
+
+	public boolean isEmitirPedidoPermitido() {
+
+		return externalContext.isUserInRole("ADMINISTRADORES")
+				|| externalContext.isUserInRole("VENDEDORES");
+	}
+
+	public boolean isCancelarPedidoPermitido() {
+
+		return externalContext.isUserInRole("ADMINISTRADORES")
+				|| externalContext.isUserInRole("VENDEDORES");
 	}
 
 }
