77659bcb90315fe17253a460b5d0ed72685e74d3
tazio.fernandes <tazio.fernandes@gmail.com>
2016-07-04 16:07:11 -0300

    Ajustes na edição de clientes

--- PedidoVenda/src/main/java/com/algaworks/cursojavaee/controller/CadastroClienteBean.java
+++ PedidoVenda/src/main/java/com/algaworks/cursojavaee/controller/CadastroClienteBean.java
@@ -16,113 +16,112 @@ import com.algaworks.cursojavaee.util.jsf.FacesUtil;
 
 @Named
 @ViewScoped
-public class CadastroClienteBean implements Serializable {	
+public class CadastroClienteBean implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
 
 	private Cliente cliente;
-	
+
 	@Inject
-	private CadastroClienteService cadastroClienteService;	
-	
-	private boolean cpf; 
-	private boolean cnpj; 
+	private CadastroClienteService cadastroClienteService;
+
+	private boolean cpf;
+	private boolean cnpj;
 	private HtmlSelectOneRadio radio;
-	
-	
-	
+
 	public CadastroClienteBean() {
 		limpar();
-		
-	}
-	
-	//Adicionar um inicalizar para que possa haver a edição do Usuário	
-		public void inicializar() {
-			if (this.cliente == null) {
-				
-			}
-		}
 
-	public void cadastrar(){
-		try{
-			
-		this.cliente = cadastroClienteService.salvar(this.cliente);		
-		FacesUtil.addInforMessage("Usuário criado com sucesso!");
-		limpar();
-		
-		}catch(NegocioException ne){
-			FacesUtil.addErrorMessage(ne.getMessage());
-		} 
 	}
-	
-	
-	public void carregarInputDocumentoIdentificacao(){
-		
-		if(this.radio.getValue().toString().equals("FISICA")){
+
+	// Adicionar um inicalizar para que possa haver a edição do Usuário
+	public void inicializar() {
+		if (this.cliente == null) {
+
+		}
+	}
+
+	public void cadastrar() {
+		try {
+
+			this.cliente = cadastroClienteService.salvar(this.cliente);
+			FacesUtil.addInforMessage("Usuário criado com sucesso!");
+			limpar();
+
+		} catch (NegocioException ne) {
+			FacesUtil.addErrorMessage(ne.getMessage());
+		}
+	}
+
+	public void carregarInputDocumentoIdentificacao() {
+
+		if (this.radio.getValue().toString().equals("FISICA")) {
 			cliente.setTipo(TipoPessoa.FISICA);
-			this.cpf=true;
-			this.cnpj=false;
-		}else if (this.radio.getValue().toString().equals("JURIDICA")){
+			this.cpf = true;
+			this.cnpj = false;
+		} else if (this.radio.getValue().toString().equals("JURIDICA")) {
 			this.cliente.setTipo(TipoPessoa.JURIDICA);
 			this.cnpj = true;
 			this.cpf = false;
 		}
-		
+
 	}
-	
-	//Pegando o valor da radiobutton e colocando numa variável
-	public void pegarDadosEscolhido( ValueChangeEvent event ){
-		  radio = (HtmlSelectOneRadio)event.getComponent();  
-		  
-		 // String nome = radio.getValue().toString();		  
-		 // System.out.println("Valor selecionado foi: " + nome);
-		  
+
+	// Pegando o valor da radiobutton e colocando numa variável
+	public void pegarDadosEscolhido(ValueChangeEvent event) {
+		radio = (HtmlSelectOneRadio) event.getComponent();
+
+		// String nome = radio.getValue().toString();
+		// System.out.println("Valor selecionado foi: " + nome);
+
 	}
-	
-	
-		
-	//Exibir na tela os enuns
-	
-	public TipoPessoa[] getTipoPessoa(){
+
+	// Exibir na tela os enuns
+
+	public TipoPessoa[] getTipoPessoa() {
 		return TipoPessoa.values();
 	}
-	
-	
-	public void limpar(){
+
+	public void limpar() {
 		cliente = new Cliente();
-		 cpf = false; 
-		 cnpj = false; 
-		 radio= null;	
+		cpf = false;
+		cnpj = false;
+		radio = null;
 	}
 
-	
+	public boolean isEditando() {
+		if (this.cliente.getDocumntoReceitaFederal().length() <= 14
+				&& isMaiorZero()) {
+			this.cpf = true;
+			this.cnpj = false;
+		} else if (isMaiorZero()
+				&& this.cliente.getDocumntoReceitaFederal().length()> 14) {
+			this.cpf = false;
+			this.cnpj = true;
+		}
+		return this.cliente.getId() != null;
+	}	
 
-	public boolean isEditando(){
-		return this.cliente.getId() !=null;
+	public boolean isMaiorZero() {
+		return this.cliente.getDocumntoReceitaFederal().length() > 0;
 	}
-
 
 	public boolean isCnpj() {
 		return cnpj;
 	}
 
-
 	public void setCnpj(boolean cnpj) {
 		this.cnpj = cnpj;
 	}
-
 
 	public boolean isCpf() {
 		return cpf;
 	}
 
-
 	public void setCpf(boolean cpf) {
 		this.cpf = cpf;
 	}
-	
-	
-	//G&S
+
+	// G&S
 	public Cliente getCliente() {
 		return cliente;
 	}
@@ -130,17 +129,13 @@ public class CadastroClienteBean implements Serializable {
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
-		
 
 	public HtmlSelectOneRadio getRadio() {
 		return radio;
 	}
 
-
-
 	public void setRadio(HtmlSelectOneRadio radio) {
 		this.radio = radio;
 	}
-	
 
 }
