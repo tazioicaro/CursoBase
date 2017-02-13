8d0b7406e736eac72f2c190add8a570f0ce61b48
tazio.fernandes <tazio.fernandes@gmail.com>
2017-02-13 11:26:59 -0200

    Alterado o FacesUltis para adcionar mensagem do cadastro do produto

--- PedidoVenda/src/main/java/com/algaworks/cursojavaee/util/jsf/FacesUtil.java
+++ PedidoVenda/src/main/java/com/algaworks/cursojavaee/util/jsf/FacesUtil.java
@@ -7,39 +7,37 @@ import javax.faces.context.FacesContext;
 
 //Classe utilitária para tratar exceções para as telas JSF
 public class FacesUtil {
-	
-	public static void addErrorMessage(String message){
-		
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
-				message, message));
-		
+
+	public static void addErrorMessage(String message) {
+
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_ERROR, message, message));
+
 	}
-	
-	//Verificar se é PostBack
-	
-	public static boolean isPostback(){
+
+	// Verificar se é PostBack
+
+	public static boolean isPostback() {
 		return FacesContext.getCurrentInstance().isPostback();
 	}
-	
-	public static boolean isNotPostBack(){
-		
+
+	public static boolean isNotPostBack() {
+
 		return !isPostback();
-		
+
 	}
-	
-	public static void addInforMessage(String message){
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, message, message));
-	
-	
+
+	public static void addInforMessage(String message) {
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, message, message));
+
 	}
-	
-	public static void addInforProductSucess(String produto, BigDecimal valor){
-		
-		
-		String msg = "Produto '" + produto+ "' cadastrado com sucesso com preço R$ " + valor + "!";
-		FacesContext.getCurrentInstance().addMessage(null, 
-				new FacesMessage(FacesMessage.SEVERITY_INFO, msg, msg));
-		
+
+	public static void addInforProductSucess(String produto, BigDecimal valor) {
+
+		String msg = "Produto '" + produto + "' cadastrado com sucesso com preço R$ " + valor + "!";
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, msg, msg));
+
 	}
 
 }
